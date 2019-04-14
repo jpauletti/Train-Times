2aa05236f21481714da3cdd578ca5763b82c3a0f
Julia Pauletti <julia.pauletti@yahoo.com>
2019-04-14 10:17:29 -0500

    interval updates and delete functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-var app = {
-
-};
-
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAZNqwk-JxwrCqdh9g4s7VpdRupifpIydw",
@@ -55,15 +51,104 @@ database.ref().on("child_added", function (childSnapshot) {
     var firstTrain = sv.firstTrain;
     var frequency = sv.frequency;
 
+    // first train time
+    var militaryTime = "HH:mm";
+    var convertedFirstTrain = moment(firstTrain, militaryTime).subtract(1, "years"); // move back a year so it's always before the current time
 
-    var nameTd = $("<td>").text(name);
-    var destinationTd = $("<td>").text(destination);
-    var frequencyTd = $("<td>").text(frequency);
-    var nextArrivalTd = $("<td>").text("05:35 PM");
-    var minutesAwayTd = $("<td>").text("10");
+    // difference from first train time to now - in minutes
+    var timeDifference = moment().diff(moment(convertedFirstTrain), "minutes"); // 
+    
+    // remainder for time apart
+    var remainder = timeDifference % frequency;
 
-    var newRow = $("<tr>").append(nameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd);
+    // minutes until next train
+    var minutesAway = frequency - remainder;
+
+    // next train time
+    var nextTrain = moment().add(minutesAway, "minutes");
+    var nextTrainTime = moment(nextTrain).format(militaryTime);
+
+    // add all to page
+    var nameTd = $("<td>").text(name).addClass("name");
+    var destinationTd = $("<td>").text(destination).addClass("destination");
+    var frequencyTd = $("<td>").text(frequency).addClass("frequency");
+    var nextArrivalTd = $("<td>").text(nextTrainTime).addClass("next-arrival");
+    var minutesAwayTd = $("<td>").text(minutesAway).addClass("minutes-away");
+
+    // var editBtn = $("<td>").html('<i class="fas fa-edit"></i>');
+    var btns = $("<td>").html('<i class="fas fa-edit"></i><i class="fas fa-trash-alt"></i>');
+
+    var newRow = $("<tr>").append(nameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd, btns);
     newRow.attr("data-id", key);
 
     $("#train-list").append(newRow);
+
+}, function (errorObject) {
+    console.log("an error occurred");
+    console.log(errorObject.code);
 })
+
+
+
+
+
+
+
+
+
+function updateArrivals () {
+    database.ref().once("value").then(function (snapshot) {
+        console.log("updating");
+        // loops through each child (each train)
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            var childData = childSnapshot.val();
+
+            var firstTrain = childData.firstTrain;
+            var frequency = childData.frequency;
+
+            // first train time
+            var militaryTime = "HH:mm";
+            var convertedFirstTrain = moment(firstTrain, militaryTime).subtract(1, "years"); // move back a year so it's always before the current time
+
+            // difference from first train time to now - in minutes
+            var timeDifference = moment().diff(moment(convertedFirstTrain), "minutes"); // 
+
+            // remainder for time apart
+            var remainder = timeDifference % frequency;
+
+            // minutes until next train
+            var minutesAway = frequency - remainder;
+
+            // next train time
+            var nextTrain = moment().add(minutesAway, "minutes");
+            var nextTrainTime = moment(nextTrain).format(militaryTime);
+
+            // update next arrival and minutes away on page
+            $('tr[data-id="' + key + '"] > .next-arrival').text(nextTrainTime);
+            $('tr[data-id="' + key + '"] > .minutes-away').text(minutesAway);
+
+        })
+
+
+
+
+
+        
+    });
+}
+
+var updateTimes = setInterval(updateArrivals, 1000*60);
+
+
+// delete trains
+$(document).on("click", ".fa-trash-alt", function (event) {
+    // get id for this train
+    var id = $(this).parent().parent().attr("data-id");
+
+    // remove train from DB
+    database.ref(id).remove();
+
+    // remove train from page
+    $(this).parent().parent().remove();
+});
